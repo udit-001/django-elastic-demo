0e6007b9ac03a34dbed3fed712a762fdd60024d4
Shekhar <shekharnunia@gmail.com>
2020-02-18 17:12:49 +0530

    upload file path changed

--- loc_search/articles/utils.py
+++ loc_search/articles/utils.py
@@ -1,4 +1,4 @@
 from django.utils.text import slugify
 
 def expert_article_upload_path(instance, filename): 
-    return f"expert-article/user_{instance.created_by.user.username}/{filename}"
+    return f"expert-article/user_{instance.created_by.username}/{filename}"
