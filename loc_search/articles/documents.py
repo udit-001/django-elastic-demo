80b1fd4e9b3086e72ef57ee686587ee7459b6e71
Shekhar <shekharnunia@gmail.com>
2020-02-18 17:36:11 +0530

    analyzer removed from index file

--- loc_search/articles/documents.py
+++ loc_search/articles/documents.py
@@ -4,13 +4,6 @@ from .models import ExpertArticle
 from tags.models import CustomTag
 
 
-html_strip = analyzer(
-    'html_strip',
-    tokenizer="standard",
-    filter=["lowercase", "stop", "snowball"],
-    char_filter=["html_strip"]
-)
-
 @registry.register_document
 class ExpertArticleDocument(Document):
 
